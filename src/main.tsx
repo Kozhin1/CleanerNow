c37ddcf5c77bf3803529033253aaf8aee487fa44
Kozhin Fatah <kozhin.fatah@pfizer.com>
2025-01-21 05:00:54 +0000

    beta v6

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'react-hot-toast';
 import App from './App';
@@ -18,7 +18,7 @@ const queryClient = new QueryClient({
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <AuthProvider>
           <App />
@@ -34,6 +34,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           />
         </AuthProvider>
       </QueryClientProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
